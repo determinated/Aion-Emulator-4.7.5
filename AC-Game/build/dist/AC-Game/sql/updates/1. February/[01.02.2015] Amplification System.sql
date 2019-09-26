313c750524d64d4c00fb7e032984768a7705ae0f
Charly Lamothe <charly.lamothe@sfr.fr>
2019-09-26 17:34:27 +0200

    Untrack useless files and update git ignore

--- AC-Game/build/dist/AC-Game/sql/updates/1. February/[01.02.2015] Amplification System.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `inventory` ADD COLUMN `is_amplified` TINYINT NOT NULL DEFAULT 0 AFTER `is_packed`;
-ALTER TABLE `inventory` ADD COLUMN `buff_skill` INT NOT NULL DEFAULT 0 AFTER `is_amplified`;
