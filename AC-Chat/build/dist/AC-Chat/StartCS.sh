f31da31ddf2fee342a9fa182c94fbedccbc7a7f3
Charly Lamothe <charly.lamothe@sfr.fr>
2019-09-26 17:41:21 +0200

    Remove useless build files and add libs folders

--- AC-Chat/build/dist/AC-Chat/StartCS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-err=1
-until [ $err == 0 ];
-do
-	java -Xms128m -Xmx128m -ea -Xbootclasspath/p:./libs/jsr166.jar -cp ./libs/*:ac-hat.jar com.aionemu.chatserver.ChatServer
-	err=$?
-done
--- AC-Login/build/dist/AC-Login/StopLS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e loginserver.pid ]
-then
-  lspid=`cat loginserver.pid`
-  kill ${lspid}
-  echo "LoginServer stop signal sent."
-else
-  echo "LoginServer is not running."
-fi
