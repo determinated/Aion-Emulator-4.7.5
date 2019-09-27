bf7dabee60ada7d3163e006c8b206ab3647202d2
Alison Carvalho <jasongunsar15@gmail.com>
2019-09-27 12:56:20 -0300

    Create Launcher.bat

---
+++ AC-Tools/No-IP/Launcher.bat
@@ -0,0 +1,1 @@
+start bin32\aion.bin -ip:127.0.0.1 -port:2106 -cc:1 -lang:enu -noweb -nowebshop -nokicks -ncg -noauthgg -ls -charnamemenu -ingameshop -DEVMODE "con_disable_console 0" -DEVMODE "g_chatlog 1" -DEVMODE "g_freefly 1" -DEVMODE "g_auto_disconnect 0"
