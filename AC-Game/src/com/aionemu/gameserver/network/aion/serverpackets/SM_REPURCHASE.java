1016f448ae078f42ffb1e87a257a3968b4d787fb
Alison Carvalho <jasongunsar15@gmail.com>
2019-11-12 16:23:50 -0300

    Update SM_REPURCHASE.java

--- AC-Game/src/com/aionemu/gameserver/network/aion/serverpackets/SM_REPURCHASE.java
+++ AC-Game/src/com/aionemu/gameserver/network/aion/serverpackets/SM_REPURCHASE.java
@@ -1,35 +1,4 @@
-/**
- * This file is part of Aion-Lightning <aion-lightning.org>.
- *
- *  Aion-Lightning is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Aion-Lightning is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details. *
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Aion-Lightning.
- *  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * Credits goes to all Open Source Core Developer Groups listed below
- * Please do not change here something, regarding the developer credits, except the "developed by XXXX".
- * Even if you edit a lot of files in this source, you still have no rights to call it as "your Core".
- * Everybody knows that this Emulator Core was developed by Aion Lightning 
- * @-Aion-Unique-
- * @-Aion-Lightning
- * @Aion-Engine
- * @Aion-Extreme
- * @Aion-NextGen
- * @Aion-Core Dev.
- */
 package com.aionemu.gameserver.network.aion.serverpackets;
-
-import java.util.Collection;
 
 import com.aionemu.gameserver.model.gameobjects.Item;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
@@ -40,8 +9,11 @@ import com.aionemu.gameserver.network.aion.AionServerPacket;
 import com.aionemu.gameserver.network.aion.iteminfo.ItemInfoBlob;
 import com.aionemu.gameserver.services.RepurchaseService;
 
+import java.util.Collection;
+
 /**
  * @author xTz, KID
+ * @author Skunkworks
  */
 public class SM_REPURCHASE extends AionServerPacket {
 
@@ -60,8 +32,9 @@ public class SM_REPURCHASE extends AionServerPacket {
     	PacketLoggerService.getInstance().logPacketSM(this.getPacketName());
         writeD(targetObjectId);
         writeD(1);
-        writeH(items.size());
+        writeH(Math.min(items.size(), 10));
 
+        int i = 0;
         for (Item item : items) {
             ItemTemplate itemTemplate = item.getItemTemplate();
 
@@ -73,6 +46,10 @@ public class SM_REPURCHASE extends AionServerPacket {
             itemInfoBlob.writeMe(getBuf());
 
             writeQ(item.getRepurchasePrice());
+
+            i++;
+            if (i == 10)
+                break;
         }
     }
 }
